f25f3cb0ff9b520a53c7489af1dc3cb15dfd491f
Lukas Grajewsky <grajewski.lukasz@wp.pl>
2019-11-01 12:17:16 +0100

    Add readme, composer, license, phpunit files templates

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,20 +4,51 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the fabulous ${chalk.red('generator-laravel-package-scaffolding')} generator!`)
+      yosay(`Welcome to ${chalk.red('generator-laravel-package-scaffolding')} generator!`)
     );
 
-    const prompts = [
+    const prompts = await this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+        type: "input",
+        name: "name",
+        message: "Your name",
+        default: ""
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Your email",
+        default: ""
+      },
+      {
+        type: "input",
+        name: "project_name",
+        message: "Your project name",
+        default: "awesome-package"
+      },
+      {
+        type: "input",
+        name: "project_desc",
+        message: "Your project description",
+        default: "Laravel package"
+      },
+      {
+        type: "input",
+        name: "php_version",
+        message: "PHP version",
+        default: "7.2"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "Your project license",
+        default: "Apache 2.0"
+      },
+    ]);
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -26,9 +57,35 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    this.fs.copyTpl(
+      this.templatePath('templates/composerfile.template'),
+      this.destinationPath('composer.json'),
+      {
+        author: this.props.name,
+        author_email: this.props.email,
+        name: this.props.project_name,
+        desc: this.props.project_desc,
+        license: this.props.license,
+        php_version: this.props.php_version,
+      }
+    );
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('files/helpers.php'),
+      this.destinationPath('src/helpers.php')
+    );
+    this.fs.copy(
+      this.templatePath('files/phpunit.xml'),
+      this.destinationPath('phpunit.xml')
+    );
+    this.fs.copyTpl(
+      this.templatePath('templates/README.md'),
+      this.destinationPath('README.md'), {
+        name: this.props.name
+      }
+    );
+    this.fs.copy(
+      this.templatePath('files/APACHE20-LICENSE'),
+      this.destinationPath('LICENSE')
     );
   }
 
---
+++ generators/app/templates/README.md
@@ -0,0 +1,10 @@
+# <%=name%>
+
+## Install
+```> php composer.phar install ```
+
+## Run tests
+```> ./vendor/bin/phpunit ```
+
+## Run PHPStan
+```> ./vendor/bin/phpstan analyse -l 7 src tests ```
